fa0d6439c98995496e33ca70fec9354254943a6f
Virajkunchala <kunchala.raju30@gmail.com>
2025-03-12 23:13:13 +0300

    test: added failing test case for Conjured items

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -10,6 +10,13 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
+        
+    def test_quality_degrades_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 3, 6)] 
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].quality)
+        self.assertEqual(2, items[0].sell_in)
 
         
 if __name__ == '__main__':
