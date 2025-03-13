d7230a3b339d9928c088d682b58e8dd5cd056268
Virajkunchala <kunchala.raju30@gmail.com>
2025-03-13 17:14:15 +0300

    test:Added test to ensure Sulfuras quality and sell_in remain constant

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -86,6 +86,17 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(-1, items[0].sell_in)
         self.assertEqual(0, items[0].quality)
         
+    def test_sulfuras_never_changes(self):
+        """
+        Test that Sulfuras, Hand of Ragnaros, never changes in quality or sell_in value.
+        Sulfuras is a legendary item and does not degrade or expire.
+        """
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+        
     def test_quality_degrades_twice_as_fast(self):
         items = [Item("Conjured Mana Cake", 3, 6)] 
         gilded_rose = GildedRose(items)
