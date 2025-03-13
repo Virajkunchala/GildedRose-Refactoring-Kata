8207112e52442e0981f5936526dd024892ef64b3
Virajkunchala <kunchala.raju30@gmail.com>
2025-03-13 16:49:56 +0300

    chore: removed placeholder test_foo

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,6 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
         
     def test_quality_degrades_twice_as_fast(self):
         items = [Item("Conjured Mana Cake", 3, 6)] 
