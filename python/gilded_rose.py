676df598ddb5201afdd74baa72d8485ecea81fc9
Virajkunchala <kunchala.raju30@gmail.com>
2025-03-13 16:37:16 +0300

    feat: added factory method and refactored GildedRose to use item updaters

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,60 +1,5 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        #code smell 1: long method
-        #The update_quality function is handling a lot of different items with
-        #very different behaviors. we need to refactor this function for better scalability and readability.
-        #We can refactor this function by breaking it down into smaller functions that handle each item type separately.
-        for item in self.items:
-            #code smell 2: multiple if statements
-            #Too many if statements can make the code hard to read and maintain.
-            #We could refactor this into individual methods or classes per item type.
-            
-            # Code Smell 3: Difficult to extend
-            # For instance, adding a new item type like 'Conjured' means adding more logic here.
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-                        if "Conjured" in item.name:
-                            item.quality -= 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                                
-            # Code Smell 4: Too many conditionals to decrease sell_in value
-            # The following block decreases the sell_in value, but there’s redundancy.
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-                
-            # Code Smell 5: Handle the expired items with more complexity
-            # There are many checks for whether the sell_in has expired or not, but it makes 
-            # the method hard to follow and extend.
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -120,3 +65,12 @@ def get_updater(item: Item) -> ItemUpdater:
         return NormalItem()
         
         
+class GildedRose(object):
+    
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            updater = get_updater(item)
+            updater.update(item)
